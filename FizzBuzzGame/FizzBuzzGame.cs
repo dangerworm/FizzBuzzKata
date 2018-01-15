486190e7a44e4331927d3dc7a5286ced2bbd74db
dangerworm <dangerworm@gmail.com>
2018-01-15 21:33:32 +0000

    First set of tests. Simple method call.

---
+++ FizzBuzzGame/FizzBuzzGame.cs
@@ -0,0 +1,43 @@
+﻿namespace FizzBuzzKata
+{
+    public class FizzBuzzGame
+    {
+        public string GetOutput(int value)
+        {
+            if (value < 1)
+            {
+                return "";
+            }
+
+            var fizzes = Fizzes(value);
+            var buzzes = Buzzes(value);
+
+            if (fizzes && buzzes)
+            {
+                return "FizzBuzz";
+            }
+
+            if (fizzes)
+            {
+                return "Fizz";
+            }
+
+            if (buzzes)
+            {
+                return "Buzz";
+            }
+
+            return value.ToString();
+        }
+
+        private bool Fizzes(int value)
+        {
+            return value % 3 == 0;
+        }
+
+        private bool Buzzes(int value)
+        {
+            return value % 5 == 0;
+        }
+    }
+}
---
+++ FizzBuzzKata/FizzBuzzTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzKata
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void GetOutput_0_ReturnsBlankString()
+        {
+            // Arrange
+            var game = new FizzBuzzGame();
+
+            // Act
+            var output = game.GetOutput(0);
+
+            // Assert
+            Assert.AreEqual(output, "");
+        }
+
+        [TestMethod]
+        public void GetOutput_1_Returns1()
+        {
+            // Arrange
+            var game = new FizzBuzzGame();
+
+            // Act
+            var output = game.GetOutput(1);
+
+            // Assert
+            Assert.AreEqual(output, "1");
+        }
+
+
+        [TestMethod]
+        public void GetOutput_2_Returns2()
+        {
+            // Arrange
+            var game = new FizzBuzzGame();
+
+            // Act
+            var output = game.GetOutput(2);
+
+            // Assert
+            Assert.AreEqual(output, "2");
+        }
+
+        [TestMethod]
+        public void GetOutput_3_ReturnsFizz()
+        {
+            // Arrange
+            var game = new FizzBuzzGame();
+
+            // Act
+            var output = game.GetOutput(3);
+
+            // Assert
+            Assert.AreEqual(output, "Fizz");
+        }
+
+        [TestMethod]
+        public void GetOutput_4_Returns4()
+        {
+            // Arrange
+            var game = new FizzBuzzGame();
+
+            // Act
+            var output = game.GetOutput(4);
+            
+            // Assert
+            Assert.AreEqual(output, "4");
+        }
+
+        [TestMethod]
+        public void GetOutput_5_ReturnsBuzz()
+        {
+            // Arrange
+            var game = new FizzBuzzGame();
+
+            // Act
+            var output = game.GetOutput(5);
+
+            // Assert
+            Assert.AreEqual(output, "Buzz");
+        }
+
+        [TestMethod]
+        public void GetOutput_15_ReturnsFizzBuzz()
+        {
+            // Arrange
+            var game = new FizzBuzzGame();
+
+            // Act
+            var output = game.GetOutput(15);
+
+            // Assert
+            Assert.AreEqual(output, "FizzBuzz");
+        }
+    }
+}
